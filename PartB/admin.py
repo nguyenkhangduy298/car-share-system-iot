95d3838764fdb45b8c1997b0dc2501251c79cc3f
Khoi <s3694615@rmit.edu.vn>
2020-09-07 23:54:04 +0700

    updated: input reported car location for email

--- PartB/admin.py
+++ PartB/admin.py
@@ -1,12 +1,13 @@
-from flask import Blueprint, render_template, session, redirect, url_for, request
+from flask import Blueprint, render_template, session, redirect, url_for, request, flash
 from email.mime.text import MIMEText
+from geopy.geocoders import Nominatim
 import smtplib
 import ssl
 
-admin = Blueprint("admin", __name__, template_folder="templates")
+adminbp = Blueprint("adminbp", __name__, template_folder="templates")
 
 
-@admin.route("/", methods=["GET"])
+@adminbp.route("/", methods=["GET"])
 def adminHome():
     """
     Routing to admin's page
@@ -17,34 +18,55 @@ def adminHome():
         return redirect(url_for("login"))
 
 
-@admin.route("/send", methods = ["GET", "POST"])
+@adminbp.route("/send", methods=["GET","POST"])
 def sendEmail():
     """
-    Send email to engineer when admin report a car
+    Send email to engineers when admin report a car
     """
     if ("user" in session) and (session["position"] == "admin"):
+        from main import ReportedCar, db
+
         if request.method == "POST":
+            # Email configuration
             port = 587
             smtp_server = "smtp.office365.com"
-            sender = "s3694615@rmit.edu.vn"
             receivers = ["s3694615@rmit.edu.vn", "dkhoilaska@gmail.com"]
+            sender = request.form["email"]
             password = request.form["password"]
+            location = request.form["location"]
 
-            message = request.form["content"]
+            # Use geopy library to get location latitude, longitude
+            try:
+                geolocator = Nominatim(user_agent="app")
+                place = geolocator.geocode(location)
+                car_location = ReportedCar(location, place.latitude, place.longitude)
+                db.session.add(car_location)
+            except (AttributeError):
+                flash("Cannot find the specified location")
+                return redirect(url_for("adminbp.sendEmail"))
+
+            # Compose email
+            message = "Car needs maintenance at: " + location + "\n(" + place.address + ")"
             mail = MIMEText(message)
             mail["Subject"] = "Testing message"
             mail["To"] = ", ".join(receivers)
+
+            # Send email
             try:
                 context = ssl.create_default_context()
                 with smtplib.SMTP(smtp_server, port) as server:
                     server.ehlo()
                     server.starttls(context=context)
                     server.login(sender, password)
+
+                    db.session.commit()  # Only store to database when login successfully
                     server.sendmail(sender, receivers, mail.as_string())
             except (smtplib.SMTPAuthenticationError):
-                return "Wrong username or password"
-            return "Emails are sent"
+                flash("Wrong username or password")
+                return redirect(url_for("adminbp.sendEmail"))
+            flash("Emails are sent to engineers")
+            return redirect(url_for("adminbp.sendEmail"))
         else:
-            return render_template("email.html")
+            return render_template("report.html")
     else:
         return redirect(url_for("login"))
