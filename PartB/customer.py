5afcc2fce63d4cbbb8feb32df70282955b2e5672
thanh-nhan1311 <60431802+thanh-nhan1311@users.noreply.github.com>
2020-09-18 13:36:14 +0700

    have image

--- PartB/customer.py
+++ PartB/customer.py
@@ -3,16 +3,17 @@ from email.mime.text import MIMEText
 from geopy.geocoders import Nominatim
 import smtplib
 import ssl
+import os
 
 customerbp = Blueprint("customerbp", __name__, template_folder="templates")
-
-
+#PEOPLE_FOLDER = os.path.join('static', 'people_photo')
 @customerbp.route("/", methods=["GET"])
 def customerHome():
     """
     Routing to customer's page
     """
     if ("user" in session) and (session["position"] == "customer"):
-        return render_template("customers.html")
+        #full_filename = os.path.join('images/product-6-720x480.jpg')
+        return render_template("booking.html")
     else:
         return redirect(url_for("login"))
