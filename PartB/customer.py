48d2254ee415ba8ff941291268150708ae395a33
thanh-nhan1311 <60431802+thanh-nhan1311@users.noreply.github.com>
2020-09-19 16:50:33 +0700

    can show user

--- PartB/customer.py
+++ PartB/customer.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request, flash
+from flask_login import login_required, current_user
 from email.mime.text import MIMEText
 from geopy.geocoders import Nominatim
 import smtplib
@@ -8,12 +9,29 @@ import os
 customerbp = Blueprint("customerbp", __name__, template_folder="templates")
 #PEOPLE_FOLDER = os.path.join('static', 'people_photo')
 @customerbp.route("/", methods=["GET"])
-def customerHome():
+def user():
     """
     Routing to customer's page
     """
     if ("user" in session) and (session["position"] == "customer"):
-        #full_filename = os.path.join('images/product-6-720x480.jpg')
-        return render_template("booking.html")
+        user = session["user"]
+        return render_template("booking.html", user = user)
     else:
         return redirect(url_for("login"))
+@customerbp.route("/contact", methods=["GET"])
+def customerContact():
+    """
+    Routing to customer's page
+    """
+    if ("user" in session) and (session["position"] == "customer"):
+        return render_template("contactus.html")
+    else:
+        return redirect(url_for("login"))
+
+from flask_login import login_user
+
+...
+@customerbp.route('/login', methods=['POST'])
+def login_post():
+    ...
+    return redirect(url_for('main.profile'))
