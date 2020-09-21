86791f4e2b73d7d52d9688fd8041282f269e4b39
Khoi7 <s3694615@rmit.edu.vn>
2020-09-22 03:47:57 +0700

    autodoc mock imports

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
@@ -49,3 +49,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+autodoc_mock_imports = ['database_utils', 'googleapiclient', 'httplib2',
+                            'oauth2client', 'admin', 'engineer', 'customer',
+                            'manager']
