8d3a8bc3cf1ed8822b825195008a104dcce91b19
Khoi7 <s3694615@rmit.edu.vn>
2020-09-21 18:48:02 +0700

    done class Person

--- PartB/main.py
+++ PartB/main.py
@@ -31,23 +31,47 @@ db = SQLAlchemy(app)
 googlemaps = GoogleMaps(app)
 
 
-class Login(db.Model):
+class Person(db.Model):
     """
-    Represent the Executive table of remote database
+        Represent the Person table of remote database
 
-    This is used to store the login credentials and position
-    information of all registered executives. These data helps
-    the process of logging in to the system as an executive.
+        This is used to store the login credentials and position
+        information of all person. These data helps
+        the process of logging in to the system.
     """
-    __tablename__ = "Login"
+    __tablename__ = "Person"
     ID = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100))
-    password = db.Column(db.String(100))
-    position = db.Column(db.String(100))
+    username = db.Column(db.Text)
+    password = db.Column(db.Text)
+    position = db.Column(db.Integer)
+    name = db.Column(db.Text)
+    address = db.Column(db.Text)
+    phone = db.Column(db.Text)
+    fax = db.Column(db.Text)
+    email = db.Column(db.Text)
+    contact = db.Column(db.Text)
+    image = db.Column(db.Text)
 
-    def __init__(self, username, password, position):
+    def __init__(self, username, password, position, name, address, phone, fax, email, contact, image):
         self.username = username
         self.password = password
+        self.position = position
+        self.name = name
+        self.address = address
+        self.phone = phone
+        self.fax = fax
+        self.email = email
+        self.contact = contact
+        self.image = image
+
+
+
+class Role(db.Model):
+    __tablename__ = "Role"
+    ID = db.Column(db.Integer, primary_key=True)
+    position = db.Column(db.Text)
+
+    def __init__(self, position):
         self.position = position
 
 
@@ -150,25 +174,26 @@ def login():
             username = request.form["name"]
             password = request.form["password"]
             try:
-                login = Login.query.filter_by(username=username).first()
-                if (sha256_crypt.verify(password, login.password)):
-                    session["user"] = login.username
-                    session["position"] = login.position
-                    if login.position == "admin":
-                        return redirect(url_for("adminbp.adminHome"))
-                    elif login.position == "customer":
-                        return redirect(url_for("customerbp.customerHome"))
-                    elif login.position == "manager":
-                        return redirect(url_for("managerbp.managerHome"))
-                    elif login.position == "engineer":
-                        return redirect(url_for("engineerbp.engineerHome"))
-                else:
-                    flash("Wrong password!")
-                    return render_template("login.html")
+                person = Person.query.filter_by(username=username).first()
+                print(person.position)
             except (AttributeError):
                 flash("Cannot find user! Try again")
+                return redirect(url_for("login"))
+            if (sha256_crypt.verify(password, person.password)):
+                session["user"] = person.username
+                role = Role.query.filter_by(ID=person.position).first()
+                session["position"] = role.position
+                if role.position == "admin":
+                    return redirect(url_for("adminbp.adminHome"))
+                elif role.position == "customer":
+                    return redirect(url_for("customerbp.customerHome"))
+                elif role.position == "manager":
+                    return redirect(url_for("managerbp.managerHome"))
+                elif role.position == "engineer":
+                    return redirect(url_for("engineerbp.engineerHome"))
+            else:
+                flash("Wrong password!")
                 return render_template("login.html")
-
         else:
             return render_template("login.html")
     else:
@@ -195,7 +220,7 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/registerexec", methods=["POST", "GET"])
+@app.route("/register", methods=["POST", "GET"])
 def register():
     """
     Routing to page for registering executive account
@@ -205,15 +230,27 @@ def register():
         password = request.form["password"]
         hashed_password = sha256_crypt.hash(password)
         position = request.form["position"]
-        login = Login(username, hashed_password, position)
-        db.session.add(login)
+        role = Role.query.filter_by(position=position).first()
+        positionID = role.ID
+        person = Person(username, hashed_password, positionID, "", "", "", "", "", "", "",)
+        db.session.add(person)
         db.session.commit()
         flash("Registered successfully")
-        return redirect("login")
+        return redirect(url_for("login"))
     else:
         return render_template("register.html")
 
 
 if __name__ == '__main__':
     db.create_all()
+    if Role.query.all() == []:
+        admin = Role("admin")
+        manager = Role("manager")
+        engineer = Role("engineer")
+        customer = Role("customer")
+        db.session.add(admin)
+        db.session.add(manager)
+        db.session.add(engineer)
+        db.session.add(customer)
+        db.session.commit()
     app.run(debug=True)
