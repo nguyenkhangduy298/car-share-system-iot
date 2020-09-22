b132f3fa9fb8afaafe8e3cbc542382b4398ddf27
nguyenkhangduy <nguyenkhangduy298@gmail.com>
2020-09-22 12:18:02 +0700

    commit

--- facialTest/socket_utils.py/MasterPi/master_cloud.py
+++ facialTest/socket_utils.py/MasterPi/master_cloud.py
@@ -5,7 +5,7 @@ sys.path.append("..")
 import socket_utils
 from database_utils import DatabaseUtils
 
-HOST = ""    # Empty string means to listen on all IP's on the machine, also works with IPv6.
+HOST = "0.0.0.0"    # Empty string means to listen on all IP's on the machine, also works with IPv6.
              # Note "0.0.0.0" also works but only with IPv4.
 PORT = 63000 # Port to listen on (non-privileged ports are > 1023).
 ADDRESS = (HOST, PORT)
@@ -37,12 +37,15 @@ def main():
 def menu(user):
     while(True):
         print("Welcome {}".format(user["username"]))
-        username = "".format(user["username"])
-        password = "".format(user["password"])
+        username = str(user["username"])
+        password = str(user["password"])
         with DatabaseUtils() as db:
-            for car in db.getOneCust(property,username,password):
-                print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(car[0], car[1], car[2], car[3], car[4], car[5], car[6], car[7], car[8], car[9], car[10], car[11], car[12]))
-
+            for cust in db.getOneCust(property,username,password):
+                if cust[0]=="":
+                    message = "Warning!!! User Not Identified"
+                else:
+                    message = "Autheticated!!! User Identified"
+                print(message)
 
 # Execute program.
 if __name__ == "__main__":
